591f42fd70ea607ae80969f7a3a3d8e0bdd489ca
Liqingcathy <cathy.glc@gmail.com>
2022-10-20 15:00:47 -0700

    added comment

--- eclipse-workspace/rest-web-servies/src/main/java/com/rest/webservies/restwebservies/user/UserResource.java
+++ eclipse-workspace/rest-web-servies/src/main/java/com/rest/webservies/restwebservies/user/UserResource.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+//rest api functions
+
 @RestController
 public class UserResource {
 	
